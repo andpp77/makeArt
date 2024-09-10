a0feb2ca31dc00f4fe574939f16eca4d669c5472
andpp77 <166177111+andpp77@users.noreply.github.com>
2024-09-09 21:02:25 -0300

    CRIANDO SITE

---
+++ estamparia/script.js
@@ -0,0 +1,13 @@
+function laco(){
+    let i =1;
+    while (i<=5){
+        alert(i)
+        i++
+    }
+}
+function intervalo(){
+    let i = Number(prompt("Digite o 1° numero:"))
+    let f = Number(prompt("Digite o 2° numero:"))
+    while (i <= f)
+        document.write(i + f)
+}
